396ac8d52f1e5b47c1f3aec5ce779c3c0059fb5d
Bryan Queme <bryanqueme8@gmail.com>
2021-06-09 00:49:03 -0600

    ejercicio1.java

---
+++ ejercicio1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ejercicio1 {
+    public static void main(String[] args){
+    int num1,num2,resultado;
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("ingrese un numero: ");
+    num1 = scanner.nextint();
+    System.out.print("ingrese numero ");
+    num2 =scanner.nextint();
+    resultado = num1+num2;
+    System.out.print("la suma es: "+resultado);
+
+    }
+    
+    }
+
