c20a6b2ee3f039191e24375380e127994fc3708f
Bryan Queme <bryanqueme8@gmail.com>
2021-06-09 11:48:10 -0600

    ejercicio 3

---
+++ ejercicio3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ejercicio3 {
+    public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    int n,doble,triple;
+
+    System.out.print("ingrese el numero: ");
+    n  = scanner.nextInt();
+    doble = n*2;
+    System.out.println("el doble del numero es: "+doble);
+    triple = n*3;
+    System.out.println( "el triple del numero es: " +triple);
+
+    }
+    
+    }
