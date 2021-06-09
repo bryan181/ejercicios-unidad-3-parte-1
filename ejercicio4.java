ad4c3ec28bd32e5266139442d1ee0f094f738722
Bryan Queme <bryanqueme8@gmail.com>
2021-06-09 12:30:51 -0600

    ejercicio 4

---
+++ ejercicio4.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ejercicio4 {
+    public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    int n,cuadrado,cubo;
+
+    System.out.print("ingrese el numero: ");
+    n  = scanner.nextInt();
+    cuadrado = n*n;
+    System.out.println("el cuadrado del numero es: "+cuadrado);
+    cubo = n*n*n;
+    System.out.println( "el cubo del numero es: " +cubo);
+
+    }
+    
+    }
