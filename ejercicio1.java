8ef25ce3ea939ae1fe8b1b2255bd797f835938c0
Bryan Queme <bryanqueme8@gmail.com>
2021-06-09 11:28:56 -0600

    ejercicio 2

--- ejercicio1.java
+++ ejercicio1.java
@@ -4,13 +4,12 @@ public class ejercicio1 {
     int num1,num2,resultado;
     Scanner scanner = new Scanner(System.in);
     System.out.print("ingrese un numero: ");
-    num1 = scanner.nextint();
-    System.out.print("ingrese numero ");
-    num2 =scanner.nextint();
+    num1 = scanner.nextInt();
+    System.out.print("ingrese numero: ");
+    num2 =scanner.nextInt();
     resultado = num1+num2;
     System.out.print("la suma es: "+resultado);
 
     }
     
     }
-
---
+++ ejercicio2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class ejercicio2 {
+    public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    String pais,capital;
+
+    System.out.print("escriba el pais: ");
+    pais =scanner.nextLine();
+    System.out.print("escriba la capital: ");
+    capital =scanner.nextLine();
+    System.out.print(capital);
+    
+    System.out.print( " es la capital de: " + pais);
+
+    }
+    
+    }
