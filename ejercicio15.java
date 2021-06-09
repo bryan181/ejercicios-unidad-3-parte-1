f8cc65b7b7aff1925650597b0d1e677f3b556d9f
Bryan Queme <bryanqueme8@gmail.com>
2021-06-09 15:47:56 -0600

    ejercicio 15

---
+++ ejercicio15.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class ejercicio15 {
+    public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    int n1,nulo;
+  
+    System.out.print("ingrese el primer numero: " );
+    n1  = scanner.nextInt();
+        
+
+    if ( n1 > 0 ) {
+       
+        System.out.println("es positivo");
+        
+
+    }else{
+
+        System.out.println("es negativo");
+
+
+    }
+
+    }
+    
+    }
